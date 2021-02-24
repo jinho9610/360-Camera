1eb83edb1d6af41d28e597919397e269580e8802
jinho9610 <jinho9610@naver.com>
2021-02-24 11:56:45 +0900

    대비 향상 코드 분할

--- mtcnn_video_얼굴_입_인식_랜드마크.py
+++ mtcnn_video_얼굴_입_인식_랜드마크.py
@@ -14,6 +14,7 @@ import mtcnn
 import cv2
 import time
 import os
+from contrast_increaser import *
 
 IMG_SIZE = (34, 26)  # 입 이미지의 가로, 세로 사이즈
 
@@ -36,17 +37,134 @@ def check_mouse(ori, box, face):
     ori = cv2.rectangle(
         ori, mouse_rect[0], mouse_rect[1], (255, 0, 0), 1)  # 입주변 박스 그리기
 
-    mouse = cv2.cvtColor(mouse, cv2.COLOR_BGR2GRAY)  # 입 사진 gray로 변경
+    mouse = cv2.cvtColor(
+        mouse, cv2.COLOR_BGR2GRAY)  # 입 사진 gray로 변경
     mouse_input = mouse.copy().reshape(
         (1, IMG_SIZE[1], IMG_SIZE[0], 1)).astype(np.float32) / 255
     pred = model.predict(mouse_input)
-    state = 'O %.1f' if pred > 0.1 else '- %.1f'
+    state = 'O %.1f' if pred > 0.5 else '- %.1f'
     state = state % pred
 
     cv2.putText(ori, state, (mouse_rect[0][0] + int((mouse_rect[1][0] - mouse_rect[0][0]) / 4),
                              mouse_rect[0][1] - 6), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 1)
 
     return ori
+
+
+def contrast_check_mouse(ori, box, face):
+    nose_x, nose_y = int(face[2][0]), int(face[2][1])
+    lm_x, lm_y = int(face[3][0]), int(face[3][1])
+    rm_x, rm_y = int(face[4][0]), int(face[4][1])
+
+    mouse = ori[nose_y + 6: int(box[3]), lm_x: rm_x]
+
+    mouse_rect = [(lm_x, nose_y + 6), (rm_x, int(box[3]))]
+
+    mouse = cv2.resize(mouse, dsize=(34, 26))
+    ori = cv2.rectangle(
+        ori, mouse_rect[0], mouse_rect[1], (255, 0, 0), 1)  # 입주변 박스 그리기
+
+    mouse = ct_increase(cv2.cvtColor(
+        mouse, cv2.COLOR_BGR2GRAY))  # 입 사진 gray로 변경
+    mouse_input = mouse.copy().reshape(
+        (1, IMG_SIZE[1], IMG_SIZE[0], 1)).astype(np.float32) / 255
+    pred = model.predict(mouse_input)
+    state = 'O %.1f' if pred > 0.5 else '- %.1f'
+    state = state % pred
+
+    cv2.putText(ori, state, (mouse_rect[0][0] + int((mouse_rect[1][0] - mouse_rect[0][0]) / 4),
+                             mouse_rect[0][1] - 6), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 1)
+
+    return ori
+
+
+def contrast_video_face_mouse_rec(load_data, input_video):
+    # loading data.pt file
+    embedding_list = load_data[0]
+    name_list = load_data[1]
+
+    length = int(input_video.get(cv2.CAP_PROP_FRAME_COUNT))
+    w = int(input_video.get(cv2.CAP_PROP_FRAME_WIDTH))
+    h = int(input_video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    output_video = cv2.VideoWriter(
+        'output_videos/output_mouse.avi', fourcc, 29.97, (w, h))
+
+    frame_num = 1
+    while True:
+        ret, frame = input_video.read()
+
+        if not ret:
+            print("end of video.")
+            break
+
+        print("Writing frame {} / {}".format(frame_num, length))
+        frame_num += 1
+
+        img = Image.fromarray(frame)
+        img_cropped_list, prob_list = mtcnn(img, return_prob=True)
+
+        if img_cropped_list is not None:
+            boxes, _, faces = mtcnn.detect(img, landmarks=True)
+
+            for i, prob in enumerate(prob_list):
+                if prob > 0.90:
+                    emb = resnet(img_cropped_list[i].unsqueeze(0)).detach()
+
+                    dist_list = []  # list of matched distances, minimum distance is used to identify the person
+
+                    for idx, emb_db in enumerate(embedding_list):
+                        dist = torch.dist(emb, emb_db).item()
+                        dist_list.append(dist)
+
+                    min_dist = min(dist_list)  # get minumum dist value
+                    min_dist_idx = dist_list.index(
+                        min_dist)  # get minumum dist index
+                    # get name corrosponding to minimum dist
+                    name = name_list[min_dist_idx]
+
+                    box = boxes[i]
+
+                    # original_frame = frame.copy() # storing copy of frame before drawing on it
+
+                    if min_dist < 0.90:
+                        frame = cv2.putText(frame, name + ' ' + str(round(min_dist, 3)), (int(box[0]), int(
+                            box[1])), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 1, cv2.LINE_AA)
+                    # else:
+                    #     frame = cv2.putText(frame, 'UNKNOWN', (int(box[0]), int(
+                    #         box[1])), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 1, cv2.LINE_AA)
+
+                    frame = cv2.rectangle(frame, (int(box[0]), int(
+                        box[1])), (int(box[2]), int(box[3])), (255, 0, 0), 2)
+
+                    face = faces[i]
+                    for k in range(len(face)):
+                        frame = cv2.circle(
+                            frame, (int(face[k][0]), int(face[k][1])), 1, (0, 0, 255), -1)
+
+                    frame = contrast_check_mouse(frame, box, face)
+
+        output_video.write(frame)
+
+        k = cv2.waitKey(1)
+        if k % 256 == 27:  # ESC
+            print('Esc pressed, closing...')
+            break
+
+        elif k % 256 == 32:  # space to save image
+            print('Enter your name :')
+            name = input()
+
+            # create directory if not exists
+            if not os.path.exists('photos/'+name):
+                os.mkdir('photos/'+name)
+
+            img_name = "photos/{}/{}.jpg".format(name, int(time.time()))
+            #cv2.imwrite(img_name, original_frame)
+            print(" saved: {}".format(img_name))
+
+    input_video.release()
 
 
 def video_face_mouse_rec(load_data, input_video):
@@ -141,5 +259,5 @@ def video_face_mouse_rec(load_data, input_video):
 if __name__ == '__main__':
     load_data = torch.load('data.pt')
 
-    input_video = cv2.VideoCapture('videos/02.23.525.mp4')
-    video_face_mouse_rec(load_data, input_video)
+    input_video = cv2.VideoCapture('360_img/525_02.23/02_23_525.mp4')
+    contrast_video_face_mouse_rec(load_data, input_video)
